5a0b850da6b25539e3585224124f3b46fd7a6318
suhyeon <superbleo2003@gmail.com>
2023-09-08 17:04:51 +0900

    writing word

--- src/Word.java
+++ src/Word.java
@@ -3,5 +3,29 @@ public class Word {
     private int level;
     private String word;
     private String meaning;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getLevel(){
+        return level;
+    }
+    public void setLevel(int level){
+        this.level = level;
+    }
+    public String getWord(){
+        return word;
+    }
+    public void setWord(String word){
+        this.word = word;
+    }
+    public String getMeaning(){
+        return meaning;
+    }
+    public void setMeaning(String meaning){
+        this.meaning = meaning;
+    }
 
 }

fbc35a5578a0e6b594383f5fb147a8445b37544d
suhyeon <superbleo2003@gmail.com>
2023-09-09 15:40:43 +0900

    writing WordManager

--- src/WordCRUD.java
+++ src/WordCRUD.java
@@ -1,10 +1,23 @@
-public class WordCRUD implements ICRUD{
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class WordCRUD implements ICRUD{
+    ArrayList<Word> list;
+    Scanner s;
+    WordCRUD(Scanner s){
+        list =  new ArrayList<>();
+        this.s=s;
+    }
     @Override
     public Object add() {
+        System.out.println("난이도(1,2,3) & 새 단어 입력 : ");
+        System.out.println("뜻 입력 : ");
         return null;
     }
 
+    public void addWord() {
+
+    }
     @Override
     public int update(Object obj) {
         return 0;
